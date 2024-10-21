43c2af1bfa5b776a4d9507617c3b7c80902279bf
atrifonovHHN <atrifonov@stud.hs-heilbronn.de>
2024-10-21 14:51:37 +0200

    added copy commands to dockerfile, manifest to gradle and modifiert docker-compose

--- Leistungsnachweis/build.gradle.kts
+++ Leistungsnachweis/build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     id("java")
 }
@@ -10,11 +12,16 @@ repositories {
 }
 
 dependencies {
-    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.10.0")
-    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testImplementation("org.mockito:mockito-core:4.11.0")
-    implementation ("com.google.code.gson:gson:2.8.9")
+    implementation("com.google.code.gson:gson:2.8.9")
+}
 
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "UseTimeCalculator"
+    }
 }
 
 tasks.test {
