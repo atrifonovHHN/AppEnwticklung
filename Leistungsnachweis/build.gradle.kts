5c7feb225fa5fe4bf99121d42d1832d33468c087
atrifonovHHN <atrifonov@stud.hs-heilbronn.de>
2024-10-21 11:06:42 +0200

    Updated project

--- Leistungsnachweis/build.gradle.kts
+++ Leistungsnachweis/build.gradle.kts
@@ -10,9 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation ("com.squareup.okhttp3:mockwebserver:4.10.0")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation ("org.mockito:mockito-core:5.5.0")
+    testImplementation ("org.mockito:mockito-junit-jupiter:5.5.0")
     implementation ("com.google.code.gson:gson:2.8.9")
 
 }
